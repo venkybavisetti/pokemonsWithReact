539ae352662c5108ac19c4ee3a23c34f5efc35e9
venkatesh <58026249+venkybavisetti@users.noreply.github.com>
2020-08-25 18:36:06 +0530

    pokemon data fetching from browser

---
+++ build/precache-manifest.d8ce912a98712544bf87e2d187d029b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fa9a8ebd212004785d4db45633b1f43",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b19c24f75bc5c3666f1",
+    "url": "/static/css/main.cbbb760e.chunk.css"
+  },
+  {
+    "revision": "d68e25e3a7874384e45a",
+    "url": "/static/js/2.c159f5c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c159f5c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b19c24f75bc5c3666f1",
+    "url": "/static/js/main.37561f37.chunk.js"
+  },
+  {
+    "revision": "4226e992f1fdb71c69c7",
+    "url": "/static/js/runtime-main.a8b8c8b0.js"
+  }
+]);
